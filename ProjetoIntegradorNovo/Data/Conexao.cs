aafe246ba025dc625882e0d19bdab71fcb6c9f08
Carl Sagan <carl.sagan@almahcondos.com.br>
2025-08-29 13:55:48 -0300

    Alterações.

--- ProjetoIntegradorNovo/Data/Conexao.cs
+++ ProjetoIntegradorNovo/Data/Conexao.cs
@@ -17,3 +17,32 @@ namespace ProjetoIntegradorNovo.Data
     }
 }
 
+///////
+////CREATE DATABASE ProjetoIntegrador;
+////GO
+
+////USE ProjetoIntegrador;
+////GO
+
+
+////CREATE TABLE Aluno (
+////    Codigo INT IDENTITY(1,1) PRIMARY KEY, 
+////    Nome NVARCHAR(150) NOT NULL,
+////    Cpf CHAR(11) NOT NULL UNIQUE
+////);
+////GO
+
+
+////CREATE TABLE Historico (
+////    Codigo INT IDENTITY(1,1) PRIMARY KEY,
+////    Cpf CHAR(11) NOT NULL,               
+////    Transacao DATETIME NOT NULL DEFAULT GETDATE(), 
+////    TipoMovimentacao NVARCHAR(50) NOT NULL,       
+////    QuantidadeMovimentacao DECIMAL(18,2) NOT NULL, 
+////    SaldoAtual DECIMAL(18,2) NOT NULL,             
+
+////    CONSTRAINT FK_Historico_Aluno FOREIGN KEY (Cpf) REFERENCES Aluno(Cpf)
+////);
+////GO
+
+
